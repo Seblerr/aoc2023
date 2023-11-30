a2d02434ed0acc2fc3df95243d8e52d3ca383089
Sebastian Nilsson <seblerr@gmail.com>
2023-11-30 23:35:13 +0100

    add template

---
+++ .template/src/bin/part1.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let input = include_str!("./input1.txt");
+    let output = part1(input);
+    dbg!(output);
+}
+
+fn part1(input: &str) -> String {
+    "todo!()".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
