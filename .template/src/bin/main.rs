b8acbc318f748e9602312c0e65beb5d697e5ace5
Sebastian <seblerr@gmail.com>
2023-12-01 13:28:32 +0100

    update template to one file for both solutions

---
+++ .template/src/bin/main.rs
@@ -0,0 +1,33 @@
+fn main() {
+    let input = include_str!("./input.txt");
+    let part1_output = part1(input);
+    let part2_output = part2(input);
+    println!("Part one answer is: {}", part1_output);
+    println!("Part two answer is: {}", part2_output);
+}
+
+fn part1(input: &str) -> String {
+    "todo!()".to_string()
+}
+
+fn part2(input: &str) -> String {
+    "todo!()".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+
+    fn part1_test() {
+        let input = include_str!("./testinput1.txt");
+        let result = part1(input);
+        assert_eq!(result, "todo!()");
+    }
+
+    fn part2_test() {
+        let input = include_str!("./testinput2.txt");
+        let result = part2(input);
+        assert_eq!(result, "todo!()");
+    }
+}
